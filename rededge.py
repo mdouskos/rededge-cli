8908275b2770e5e895467534f54f4f33cd5476f8
Valsamis Ntouskos <mdouskos@gmail.com>
2021-05-11 16:21:54 +0300

    Refactor code

--- rededge.py
+++ rededge.py
@@ -10,88 +10,107 @@ from tqdm import tqdm
 
 
 #%% API Access Functions
-def build_url(host='192.168.10.254', set=0, dir=0):
-    url = 'https://' + host + '/files' + '/{:04d}SET'.format(set) + '/{:03d}'.format(dir)
+def build_url(host, set=None, dir=None):
+    url = "http://" + host + "/files"
+
+    if set is not None:
+        url += "/{:04d}SET".format(set)
+
+    if dir is not None:
+        url += "/{:03d}".format(dir)
+
     return url
+
+
+def _print_list(l):
+    _st = ""
+    for it in l:
+        _st += it + "\n"
+    print(_st)
 
 
 def process_response(url):
     resp = requests.get(url)
     if resp.status_code != 200:
-        raise ValueError("Returned {} status code for URL {}".format(resp.status_code, url))
+        raise ValueError(
+            "Returned {} status code for URL {}".format(resp.status_code, url)
+        )
     return resp
 
-def get_sets(host):
-    url = 'http://' + host + '/files'
+
+def get_dirs(host, set=None):
+    url = build_url(host, set)
     resp = process_response(url)
     data = resp.json()
-    return data['directories']
-    
+    return data["directories"]
+
+
 def get_file_list(host, set, dir):
-    url = 'http://' + host + '/files' + '/{:04d}SET'.format(set) + '/{:03d}'.format(dir)
+    url = build_url(host, set, dir)
     resp = process_response(url)
     data = resp.json()
-    file_list = [ x['name'] for x in data['files'] ]
+    file_list = [x["name"] for x in data["files"]]
     return file_list
 
-def download_set_file(host, set, dir, filename, output_path):
-    url = 'http://' + host + '/files' + '/{:04d}SET'.format(set) + '/{:03d}'.format(dir) + '/' + filename
+
+def download_setfiles(host, set, dir, filename, output_path):
+    url = build_url(host, set, dir) + "/" + filename
     resp = process_response(url)
-    with open(os.path.join(output_path, filename), 'wb') as f:
+    with open(os.path.join(output_path, filename), "wb") as f:
         f.write(resp.content)
 
-def _print_list(l):
-    _st = ''
-    for it in l:
-        _st += it + '\n'
-    print(_st)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Access MicaSense camera API.")
-    
-    parser.add_argument("--host", required=True,
-                      help="MicaSense camera hostname or IP address")
 
-    subparsers = parser.add_subparsers(dest='command')
+    parser.add_argument(
+        "--host",
+        help="MicaSense camera hostname or IP address",
+        default="192.168.10.254",
+    )
 
-    getcmd = subparsers.add_parser('get', help='Get info')
-    getcmd.add_argument('asset', help="Asset to get info for", choices=('sets', 'setfiles'))
-    getcmd.add_argument("--set", required=False, type=int,
-                    help="Set of images")
+    subparsers = parser.add_subparsers(dest="command")
 
-    getcmd.add_argument("--dir", required=False, type=int,
-                    help="Dir of images")
+    getcmd = subparsers.add_parser("get", help="Get info")
+    getcmd.add_argument(
+        "asset", help="Asset to get info for", choices=("sets", "setdirs", "setfiles")
+    )
+    getcmd.add_argument("--set", type=int, help="Set of images", default=0)
+    getcmd.add_argument("--dir", type=int, help="Dir of images", default=0)
 
-    downcmd = subparsers.add_parser('download', help='Download images')
-
-    downcmd.add_argument("--output-path", required=True,
-                      help="Path to a folder where to save images")
-    downcmd.add_argument("--set", required=True, type=int,
-                      help="Set of images")
-
-    downcmd.add_argument("--dir", required=False, type=int,
-                    help="Dir of images")
+    downcmd = subparsers.add_parser("download", help="Download images")
+    downcmd.add_argument(
+        "--output-path", required=True, help="Path to a folder where to save images"
+    )
+    downcmd.add_argument("--set", type=int, help="Set of images", default=0)
+    downcmd.add_argument(
+        "--dir", required=False, type=int, help="Dir of images", default=0
+    )
     return parser.parse_args()
 
-# %% 
+
+# %%
 if __name__ == "__main__":
     args = parse_arguments()
     host = args.host
-    if args.command == 'get':
-        if args.asset == 'sets':
-            _print_list(get_sets(host))
-        elif args.asset == 'setfiles':
-            if args.set is not None:
-                _print_list(get_file_list(host, args.set, args.dir))
-            else:
-                raise AssertionError('No set given')
-    elif args.command == 'download':
+    if args.command == "get":
+        if args.asset == "sets":
+            print("Sets available:")
+            _print_list(get_dirs(host))
+        elif args.asset == "setdirs":
+            print("Dirs of set {}:".format(args.set))
+            _print_list(get_dirs(host, args.set))
+        elif args.asset == "setfiles":
+            print("Files of set {} and dir {}:".format(args.set, args.dir))
+            _print_list(get_file_list(host, args.set, args.dir))
+    elif args.command == "download":
         output_path = args.output_path
         if not os.path.exists(output_path):
             os.makedirs(output_path)
+        print("Downloading files of set {} and dir {}".format(args.set, args.dir))
         file_list = get_file_list(host, args.set, args.dir)
         for f in tqdm(file_list):
-            download_set_file(host, args.set, args.dir, f, output_path)
+            download_setfiles(host, args.set, args.dir, f, output_path)
         print("All files have been downloaded")
-        
+
 # %%
